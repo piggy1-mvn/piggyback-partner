4665fa89d862cc74a113bee31422e26b1eb22635
unknown <elizabethjohnjk333@gmail.com>
2019-08-21 12:44:14 +0800

    Partner Source codde commit 1

---
+++ src/main/java/com/incentives/piggyback/partner/PartnerApplication.java
@@ -0,0 +1,22 @@
+package com.incentives.piggyback.partner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class PartnerApplication {
+	
+	@RequestMapping("/partnerHome")
+	public String home() {
+		return "Hello, from partner service in Docker";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerApplication.class, args);
+	}
+
+}
