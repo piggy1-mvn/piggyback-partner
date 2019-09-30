44a76784b825a45dfed1e2adfad08ea920246268
Rajan Gupta <rajan0493@gmail.com>
2019-09-30 12:43:07 +0800

    Remove Partner Order

--- src/main/java/com/incentives/piggyback/partner/adapter/ObjectAdapter.java
+++ src/main/java/com/incentives/piggyback/partner/adapter/ObjectAdapter.java
@@ -4,9 +4,7 @@ import java.util.Calendar;
 import java.util.UUID;
 
 import com.incentives.piggyback.partner.dto.PartnerEntity;
-import com.incentives.piggyback.partner.dto.PartnerOrderEntity;
 import com.incentives.piggyback.partner.entity.Partner;
-import com.incentives.piggyback.partner.entity.PartnerOrder;
 import com.incentives.piggyback.partner.util.CommonUtility;
 
 public class ObjectAdapter {
@@ -49,53 +47,4 @@ public class ObjectAdapter {
 		return currentPartner;
 	}
 
-	public static PartnerOrderEntity getPartnerOrderEntity(PartnerOrder partnerOrder) {
-		PartnerOrderEntity orderEntity = new PartnerOrderEntity();
-		orderEntity.setOrderId(partnerOrder.getOrderId());
-		orderEntity.setPartnerId(partnerOrder.getPartnerId());
-		orderEntity.setOrderType(partnerOrder.getOrderType());
-		orderEntity.setOptimizationDuration(partnerOrder.getOptimizationDuration());
-		orderEntity.setMaxOptimizations(partnerOrder.getMaxOptimizations());
-		orderEntity.setOrderLocation(partnerOrder.getOrderLocation());
-		orderEntity.setInitiatorUserId(partnerOrder.getInitiatorUserId());
-		orderEntity.setOrderStatus(partnerOrder.getOrderStatus());
-		orderEntity.setOptimizationRadius(partnerOrder.getOptimizationRadius());
-		orderEntity.setCreatedDate(Calendar.getInstance().getTime());
-		orderEntity.setLastModifiedDate(Calendar.getInstance().getTime());
-		orderEntity.setPartnerDisplayName(partnerOrder.getPartnerDisplayName());
-		orderEntity.setPartnerRedirectUrl(partnerOrder.getPartnerRedirectUrl());
-		orderEntity.setPartnerWebHookAddress(partnerOrder.getPartnerWebHookAddress());
-		orderEntity.setIsActive(1);
-		return orderEntity;
-	}
-
-	public static PartnerOrderEntity updatePartnerOrderEntity(
-			PartnerOrderEntity currentOrder, PartnerOrder modifiedOrder) {
-		if (CommonUtility.isValidString(modifiedOrder.getPartnerId()))
-			currentOrder.setPartnerId(modifiedOrder.getPartnerId());
-
-		if (CommonUtility.isValidString(modifiedOrder.getOrderType()))
-			currentOrder.setOrderType(modifiedOrder.getOrderType());
-
-		if (modifiedOrder.getOptimizationDuration() != 0)
-			currentOrder.setOptimizationDuration(modifiedOrder.getOptimizationDuration());
-
-		if (modifiedOrder.getMaxOptimizations() != 0)
-			currentOrder.setMaxOptimizations(modifiedOrder.getMaxOptimizations());
-
-		if (!CommonUtility.isNullObject(modifiedOrder.getOrderLocation()))
-			currentOrder.setOrderLocation(modifiedOrder.getOrderLocation());
-
-		if (CommonUtility.isValidLong(modifiedOrder.getInitiatorUserId()))
-			currentOrder.setInitiatorUserId(modifiedOrder.getInitiatorUserId());
-
-		if (CommonUtility.isValidString(modifiedOrder.getOrderStatus()))
-			currentOrder.setOrderStatus(modifiedOrder.getOrderStatus());
-
-		if (modifiedOrder.getOptimizationRadius() != 0)
-			currentOrder.setOptimizationRadius(modifiedOrder.getOptimizationRadius());
-
-		currentOrder.setLastModifiedDate(Calendar.getInstance().getTime());
-		return currentOrder;
-	}
 }
