50df0429d3f9e9b28b6768a4a8891f2d6e40b50d
Rajan Gupta <rajan0493@gmail.com>
2019-11-02 18:53:45 +0800

    Jasypt encryption

--- src/main/java/com/incentives/piggyback/partner/PartnerApplication.java
+++ src/main/java/com/incentives/piggyback/partner/PartnerApplication.java
@@ -1,5 +1,6 @@
 package com.incentives.piggyback.partner;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,15 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class PartnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PartnerApplication.class, args);
 	}
 
-
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+
 }
